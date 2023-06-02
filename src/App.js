6fe6a436294a5f33f4abb6dfc0344196bf383f62
Soumya Dey <soumyadey200@hotmail.com>
2023-06-02 21:39:26 +0530

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,44 @@
-import logo from './logo.svg';
+import { useState } from 'react';
+import axios from 'axios';
+
 import './App.css';
 
 function App() {
+  const [barcode, setBarcode] = useState('');
+
+  const barcodeAutoFocus = () => {
+    document.getElementById('barcode').focus();
+  };
+
+  const onChangeBarcode = (event) => {
+    setBarcode(event.target.value);
+  };
+
+  const onKeyPressBarcode = async (event) => {
+    if (event.key == 'Enter') {
+      setBarcode(event.target.value);
+      alert(`Sannning Done\nBARCODE: ${event.target.value}`);
+
+      // const { data } = await axios({
+      //   method: 'post',
+      // });
+    }
+    console.log({ event });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <input
+        autoFocus={true}
+        placeholder='Start Scanning'
+        value={barcode}
+        onChange={onChangeBarcode}
+        id='barcode'
+        className='barcode'
+        onKeyDown={onKeyPressBarcode}
+        onBlur={barcodeAutoFocus}
+      />
+      {/* <input type='text' id='one' onKeyDown={handleKeyPress} /> */}
     </div>
   );
 }
